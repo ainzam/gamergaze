f2e1eb059083b8267438d9369152542f932c56a5
cfgs <cfgs@pc-info-host>
2024-04-08 20:45:07 +0200

    pluh

---
+++ src/test/java/com/gamegaze/gamergaze/GamergazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamegaze.gamergaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamergazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
