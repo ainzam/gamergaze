bb10f7601ce1571ee40e6ba5b214d330532d4b61
cfgs <cfgs@pc-info-host>
2024-04-10 19:49:17 +0200

    pluh

--- src/main/java/com/gamegaze/gamergaze/GamergazeApplication.java
+++ src/main/java/com/gamegaze/gamergaze/GamergazeApplication.java
@@ -1,12 +1,13 @@
 package com.gamegaze.gamergaze;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GamergazeApplication {
 
 	public static void main(String[] args) {
-		
+		SpringApplication.run(GamergazeApplication.class, args);
 	}
 
 }
