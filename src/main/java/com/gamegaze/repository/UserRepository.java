a07e3e65b3a18105708ba5154e701e493af922ab
cfgs <cfgs@pc-info-host>
2024-04-18 20:10:25 +0200

    login casi america ya! hello

---
+++ src/main/java/com/gamegaze/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.gamegaze.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gamegaze.domain.User;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE AppUser a " +
+            "SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableUser(String email);
+
+}
