6cea0a5a8794592c0de5aa266e389a09b7652001
D6ni <daniellamerce@gmail.com>
2024-05-07 19:01:45 +0200

    js added

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+var btn = document.querySelector("#btn");
+		var sidebar = document.querySelector(".sidebar");
+		btn.onclick = function(){
+			sidebar.classList.toggle('active');
+		}
+		
+		};
