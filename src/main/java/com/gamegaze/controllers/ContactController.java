ab834f8d114dde0d78b09d36b2f7065d1616deef
ainzam <rebe5723@gmail.com>
2024-05-30 17:48:14 +0200

    UwU

--- src/main/java/com/gamegaze/controllers/ContactController.java
+++ src/main/java/com/gamegaze/controllers/ContactController.java
@@ -1,14 +1,12 @@
 package com.gamegaze.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
--- src/main/java/com/gamegaze/domain/ContactMessage.java
+++ src/main/java/com/gamegaze/domain/ContactMessage.java
@@ -1,24 +1,13 @@
 package com.gamegaze.domain;
 
-import java.util.Date;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
+@Data
 @Entity
 public class ContactMessage {
 	@Id
